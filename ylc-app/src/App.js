495f1ae576c2be1dcfdc59d381b3703dca6d8744
Tonny Ouma <tonouma@amazon.com>
2020-10-29 18:59:44 -0500

    auth profile

--- ylc-app/src/App.js
+++ ylc-app/src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+import { Button } from 'antd'
+import { Auth } from 'aws-amplify'
+import { withAuthenticator } from 'aws-amplify-react'
+import Container from './Container'
 
-import Amplify from 'aws-amplify';
-import aws_exports from './aws-exports';
-
-import { withAuthenticator } from 'aws-amplify-react';
-//import { Header } from 'semantic-ui-react';
-
-Amplify.configure(aws_exports);
-
-function App() {
+function Profile() {
+  useEffect(() => {
+    checkUser()
+  }, [])
+  const [user, setUser] = useState({}) 
+  async function checkUser() {
+    try {
+      const data = await Auth.currentUserPoolUser()
+      const userInfo = { username: data.username, ...data.attributes, }
+      setUser(userInfo)
+    } catch (err) { console.log('error: ', err) }
+  }
+  function signOut() {
+    Auth.signOut()
+      .catch(err => console.log('error signing out: ', err))
+  }
   return (
-      <h1>Hello World!</h1>
+    <Container>
+      <h1>Profile</h1>
+      <h2>Username: {user.username}</h2>
+      <h3>Email: {user.email}</h3>
+      <h4>Phone: {user.phone_number}</h4>
+      <Button onClick={signOut}>Sign Out</Button>
+    </Container>
   );
 }
 
-export default withAuthenticator(App, {
-  includeGreetings: true,
-  signUpConfig: {
-    hiddenDefaults: ['phone_number']
-  }
-});
+export default withAuthenticator(Profile)
 
 
