2d75909c08af66c5ede86876380f54f2e57b9c3a
melliemuse <melliemuse@gmail.com>
2020-10-30 15:23:26 -0500

    fixed layout issues with side navbar

--- ylc-app/src/Nav.js
+++ ylc-app/src/Nav.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import {useHistory} from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 import { Layout, Menu } from 'antd';
 
 const Nav = (props) => {
     const history = useHistory()
     const { Sider } = Layout;
-    
+
     const styles = {
+        nav: {
+            // marginTop: '40px',
+            height: '100%'
+        },
         menuItem: {
             paddingTop: '10px',
             backgroundColor: '#696969',
@@ -23,33 +27,33 @@ const Nav = (props) => {
             fontSize: '1.2em',
         }
     }
-    
-        return (
+
+    return (
+        <div style={styles.nav} >
             <Sider
-            style={{
-                overflow: 'auto',
-                height: '100vh',
-                position: 'fixed',
-                left: 0,
-                backgroundColor: '#696969'
-              }}
-             trigger={null} 
-             >
-              <div className="logo" />
-              <Menu style={styles.menuItem} theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-                <Menu.Item key="1" style={styles.title}>
-                  YLC Matching
+                style={{
+                    height: '1800px',
+                    float: 'left',
+                    left: 0,
+                    backgroundColor: '#696969'
+                }}
+                trigger={null}
+            >
+                <Menu style={styles.menuItem} theme="dark" mode="vertical" defaultSelectedKeys={['1']}>
+                    <Menu.Item key="1" style={styles.title}>
+                        YLC Matching
                 </Menu.Item>
-                <Menu.Item key="2" onClick={()=> history.push('/login')} style={styles.navItem}>
-                  Admin
+                    <Menu.Item key="2" onClick={() => history.push('/login')} style={styles.navItem}>
+                        Admin
                 </Menu.Item>
-                <Menu.Item key="3" onClick={()=>history.push('/')} style={styles.navItem}>
-                  Student
+                    <Menu.Item key="3" onClick={() => history.push('/')} style={styles.navItem}>
+                        Student
                 </Menu.Item>
-              </Menu>
+                </Menu>
             </Sider>
-        );
+        </div>
+    );
 }
 
-  
+
 export default Nav
